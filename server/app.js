d591781b6b937848ca2ccd3b96ee7285d7538c38
Amy Turnah <amy@scarlettmadz.com>
2021-12-02 19:47:13 +1100

    Add capsules endpoint

--- server/app.js
+++ server/app.js
@@ -10,12 +10,27 @@ app.use(express.urlencoded({ extended: true }));
  
 app.use(cors())
 
+const spaceXAPIServie = "https://api.spacexdata.com/v3"
+
 app.get('/', async (req, res) => {
-    const rows = await dbPool.query('SELECT * FROM spaceData');
     res.status(200);
-    res.send({
-        result: JSON.stringify(rows)
-    });
+    res.send("Space Station API");
+}); 
+
+app.get('/capsules', async (req, res) => {
+    try {
+        const response = await axios.get(`${spaceXAPIServie}/capsules`)
+        const capsules = response.data
+        res.status(200);
+        res.json(capsules);
+    } 
+    catch (err) {
+        res.status(500);
+        res.send({
+            error: err.message
+        });
+    }
+    
 });
 
 app.listen('4000');

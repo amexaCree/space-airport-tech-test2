3454e197c816ee0715e2a06d68518321362b7bc9
Amy Turnah <amy@scarlettmadz.com>
2021-12-02 19:41:09 +1100

    Remove body parser

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 const dbPool = require('./db');
-const express =require('express');
-const bodyParser = require('body-parser');
+const express = require('express');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
     const rows = await dbPool.query('SELECT * FROM spaceData');
